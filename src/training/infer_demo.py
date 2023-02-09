ad2c58e3bc2be68adcdf1977f72ed959e527842a
SFauth <SFauth@gmx.net>
2023-02-09 15:56:06 +0000

    produces html table with playable sound

--- src/training/infer_demo.py
+++ src/training/infer_demo.py
@@ -1,14 +1,16 @@
+#%%
 import sys
 sys.path.append("../.")
 import os
 import torch
 import librosa
 import glob
+import pandas as pd
 from open_clip import create_model
 from training.data import get_audio_features
 from training.data import int16_to_float32, float32_to_int16
 from transformers import RobertaTokenizer
-
+#%%
 tokenize = RobertaTokenizer.from_pretrained('roberta-base')
 def tokenizer(text):
     result = tokenize(
@@ -20,7 +22,7 @@ def tokenizer(text):
     )
     return {k: v.squeeze(0) for k, v in result.items()}
 
-def infer_text():
+def infer_text(text_data):
     device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
     precision = 'fp32'
     amodel = 'HTSAT-tiny' # or 'PANN-14'
@@ -35,17 +37,16 @@ def infer_text():
         pretrained,
         precision=precision,
         device=device,
-        enable_fusion=enable_fusion,
+        enable_fusion=False,
         fusion_type=fusion_type
     )
     # load the text, can be a list (i.e. batch size)
-    text_data = ["I love the contrastive learning", "I love the pretrain model"] 
+    #text_data = ["I love the contrastive learning", "I love the pretrain model"] 
     # tokenize for roberta, if you want to tokenize for another text encoder, please refer to data.py#L43-90 
     text_data = tokenizer(text_data)
     
     text_embed = model.get_text_embedding(text_data)
     print(text_embed.size())
-
     return text_embed
 
 def infer_audio():
@@ -64,44 +65,89 @@ def infer_audio():
         pretrained,
         precision=precision,
         device=device,
-        enable_fusion=enable_fusion,
+        enable_fusion=False,
         fusion_type=fusion_type
     )
 
     # load the waveform of the shape (T,), should resample to 48000
     file_list = glob.glob("/home/sfauth/code/CLAP/data/clotho_test_data/*.wav")
+    print(file_list[0:5])
+    file = file_list[4]
 
-    file_list = file_list[0:2]
+    audio_waveform, sr = librosa.load(file, sr=48000) 
 
-    
-    audio_dicts = []
+    # quantize
+    audio_waveform = int16_to_float32(float32_to_int16(audio_waveform))
+    audio_waveform = torch.from_numpy(audio_waveform).float()
+    audio_dict = {}
 
-    for file in file_list:
-
-        audio_waveform, sr = librosa.load(file, sr=48000) 
-        # quantize
-        audio_waveform = int16_to_float32(float32_to_int16(audio_waveform))
-        audio_waveform = torch.from_numpy(audio_waveform).float()
-        audio_dict = {}
-
-        # the 'fusion' truncate mode can be changed to 'rand_trunc' if run in unfusion mode
-        audio_dict = get_audio_features(
-            audio_dict, audio_waveform, 480000, 
-            data_truncating='rand_trunc', 
-            data_filling='repeatpad',
-            audio_cfg=model_cfg['audio_cfg']
-        )
-
-        audio_dicts.append(audio_dict)
-    
+    # the 'fusion' truncate mode can be changed to 'rand_trunc' if run in unfusion mode
+    audio_dict = get_audio_features(
+        audio_dict, audio_waveform, 480000, 
+        data_truncating='rand_trunc', 
+        data_filling='repeatpad',
+        audio_cfg=model_cfg['audio_cfg']
+    )
     # can send a list to the model, to process many audio tracks in one time (i.e. batch size)
-    audio_embed = model.get_audio_embedding(audio_dicts)
+    audio_embed = model.get_audio_embedding([audio_dict])
     print(audio_embed.size())
+
+    return audio_embed, file
     
-    return audio_embed
+
 
 if __name__ == "__main__":
 
-    text_embed = infer_text()
-    audio_embed = infer_audio()
-    print(text_embed @ audio_embed.T)
+    # 5 experiments (HTML files)
+    # Automatize getting GT caption
+ 
+    
+    audio_embed, file = infer_audio()
+
+    wav_only = os.path.split(file)[1]
+    path_to_wav = os.path.join("clotho_test_data", wav_only) # place close to html to avoid errors with not finding the file
+
+    # get GT captions
+
+    gt_captions = pd.read_csv("sim_tests/clotho_test_data/clotho_captions_evaluation.csv")
+    captions = gt_captions[gt_captions["file_name"] == wav_only].iloc[:, 3:5].stack().tolist() #take 3rd and 4th GT caption
+    
+    captions.insert(0, "I love the contrastive learning")
+    
+    caption_list = ["animal", "this is an animal", "this is an audio clip of an animal", "this is an audio clip of an anmial making noises"]
+
+    for caption in caption_list:
+
+        captions.insert(len(captions), caption)
+
+
+    text_embed = infer_text(captions) 
+    similarities = text_embed @ audio_embed.T
+    similarities = similarities.cpu().data.numpy()
+    
+    #only_wav = os.path.split(file)[1]
+    wav_col = pd.Series([file] * len(captions))
+
+    
+
+    sim_text = pd.concat([pd.DataFrame(similarities), pd.Series(captions)], axis=1)
+    sim_text = pd.concat([sim_text, wav_col], axis=1)
+    sim_text.columns = ["dot_product(text, audio.T)", "Text", "Audio"]
+
+    #sim_text["Audio"] = pd.Series([f"""<audio controls> <source src="clotho_test_data/105bpm.wav" type="audio/wav"> </audio>"""] * len(text))
+
+    print(path_to_wav)
+    sim_text["Audio"] = sim_text["Audio"].apply(lambda audio_path: f"""<audio controls> <source src="{path_to_wav}" type="audio/wav"> </audio>""")
+    
+    # save table as html
+
+    html_filename =  wav_only + ".html"
+    root_path = os.getcwd()
+    html_path = os.path.join(root_path, "sim_tests", html_filename)
+    sim_text.to_html(html_path, escape=False)
+    
+    
+    """
+    - fluctuates somewhat, but the winner stays the winner, which is what matters to us
+    """
+    
